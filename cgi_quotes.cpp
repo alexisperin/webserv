2841696b59873e5e32224581698c4c4d23404c6b
aperin <aperin@student.s19.be>
2023-06-07 13:01:49 +0200

    Change some stuff in quote adn cig

--- cgi_quotes.cpp
+++ cgi_quotes.cpp
@@ -6,28 +6,50 @@
 /*   By: aperin <aperin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 14:25:29 by aperin            #+#    #+#             */
-/*   Updated: 2023/06/06 14:38:23 by aperin           ###   ########.fr       */
+/*   Updated: 2023/06/07 11:59:54 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 int main()
 {
+
 	srand(time(0));
 	std::ifstream file("quotes.txt");
-	std::string line;
+	std::string quote;
 	int	i = (rand() % 100) + 1;
 
 	if (!file.is_open())
-		return 1; // HANDLE ERROR
+	{
+		std::cout << "HTTP/1.1 500 Internal Server Error\n\n";
+		return 1;
+	}
 	while(!file.eof() && i)
 	{
-		std::getline(file, line);
+		std::getline(file, quote);
 		i--;
 	}
-	std::cout << line << std::endl;
 	file.close();
+
+	std::string html_content;
+	std::ostringstream content_length;
+
+	html_content += "<html><head><link rel=\"stylesheet\" href=\"css/quote.css\"></head>";
+	html_content += "<body><div><div class=\"wave\"></div><div class=\"wave\"></div>";
+	html_content += "<div class=\"wave\"></div></div><div class=\"container\">";
+	html_content += "<div class=\"box\"><div class=\"text\">" + quote;
+	html_content += "<a href=\"quote.html\"><button class=\"button button1\">Next quote</button></a>";
+	html_content += "Next quote</button></div></div></body></html>";
+
+	std::string content = "HTTP/1.1 200 OK\n";
+	content_length << html_content.size();
+
+	content += "Content-Type:text/html\nContent-Length: ";
+	content += content_length.str() + "\n\n" + html_content;
+	std::cout << content << std::endl;
+	return 0;
 }
